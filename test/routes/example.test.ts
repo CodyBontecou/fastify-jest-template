766226f3105e56317634dd1b134b7950f6695eb8
Cody Bontecou <bontecouc@gmail.com>
2021-07-16 17:21:17 -1000

    initial commit

---
+++ test/routes/example.test.ts
@@ -0,0 +1,13 @@
+import { build } from '../helper'
+
+describe('example tests', () => {
+  const app = build()
+
+  test('example is loaded', async () => {
+    const res = await app.inject({
+      url: '/example',
+    })
+
+    expect(res.payload).toEqual('this is an example')
+  })
+})
